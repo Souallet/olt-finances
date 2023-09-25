35d5ebbdbcc5dc098c4c95921b4e27312e73d207
Sébastien Ouallet <sebastien.ouallet@gmail.com>
2023-09-25 14:36:33 +0200

    feat: add chart to total kpi

---
+++ src/components/charts/incomes.tsx
@@ -0,0 +1,104 @@
+import { addDays, format, getMonth } from "date-fns";
+import { fr } from "date-fns/locale";
+import {
+  LineChart,
+  Line,
+  Tooltip,
+  ResponsiveContainer,
+  XAxis,
+  YAxis,
+  ReferenceLine,
+} from "recharts";
+import {
+  Card,
+  CardContent,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "../ui/card";
+
+export default function Incomes({ movements = [] }: { movements: any[] }) {
+  const data = movements
+    .reduce((accumulator, currentValue) => {
+      const currMonth = getMonth(
+        new Date(
+          addDays(new Date(1900, 0, 0), currentValue["Date de l'opération"] - 1)
+        )
+      );
+      const currMonthName = format(new Date(2000, currMonth, 1), "MMMM", {
+        locale: fr,
+      });
+
+      const accElement = accumulator.find((e: any) => e.index === currMonth);
+
+      if (accElement) {
+        accumulator.splice(accumulator.indexOf(accElement), 1, {
+          index: currMonth,
+          month: currMonthName,
+          total: accElement.total + currentValue["Montant TTC"],
+        });
+      } else {
+        accumulator.push({
+          index: currMonth,
+          month: currMonthName,
+          total: currentValue["Montant TTC"],
+        });
+      }
+
+      return [...accumulator];
+    }, [])
+    .sort((a: any, b: any) => a.index - b.index);
+
+  const minY = Math.min(...data.map((d: any) => d.total));
+
+  return (
+    <ResponsiveContainer width="100%" height="100%">
+      <LineChart data={data}>
+        <Tooltip content={<CustomTooltip />} />
+
+        {minY < 0 && (
+          <ReferenceLine
+            y={0}
+            stroke="#2563EB"
+            strokeWidth={1}
+            strokeOpacity={0.25}
+          />
+        )}
+        <Line
+          type="natural"
+          dataKey="total"
+          stroke="#2563EB"
+          activeDot={{ r: 8 }}
+        />
+      </LineChart>
+    </ResponsiveContainer>
+  );
+}
+
+const CustomTooltip = ({
+  active,
+  payload,
+  label,
+}: {
+  active?: boolean;
+  payload?: any;
+  label?: string;
+}) => {
+  if (active && payload && payload.length) {
+    return (
+      <Card className="p-3 text-sm flex flex-col gap-3">
+        <h6 className=" text-md font-bold capitalize">
+          {payload[0].payload.month}
+        </h6>
+        <p className="label">
+          Total généré :{" "}
+          <span className="font-medium">
+            {payload[0].payload.total.toFixed(2)} €
+          </span>
+        </p>
+      </Card>
+    );
+  }
+
+  return null;
+};
--- src/components/screens/overview/kpi.tsx
+++ src/components/screens/overview/kpi.tsx
@@ -1,3 +1,4 @@
+import Incomes from "@/components/charts/incomes";
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 
 export default function Kpi({ movements = [] }: { movements: any[] }) {
@@ -6,11 +7,10 @@ export default function Kpi({ movements = [] }: { movements: any[] }) {
     0
   );
 
-  const totalIncomes = movements.reduce(
-    (acc, currentValue) =>
-      parseFloat(currentValue["Montant TTC"]) > 0
-        ? acc + parseFloat(currentValue["Montant TTC"])
-        : acc + 0,
+  const incomes = movements.filter((e) => parseFloat(e["Montant TTC"]) > 0);
+
+  const totalIncomes = incomes.reduce(
+    (acc, currentValue) => acc + parseFloat(currentValue["Montant TTC"]),
     0
   );
 
@@ -33,26 +33,16 @@ export default function Kpi({ movements = [] }: { movements: any[] }) {
 
   return (
     <div className="grid gap-4 md:grid-cols-2 lg:grid-cols-4">
-      <Card className="col-span-2 ">
+      <Card className="col-span-2  row-span-2 ">
         <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
-          <CardTitle className="text-sm font-medium">Total</CardTitle>
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            viewBox="0 0 24 24"
-            fill="none"
-            stroke="currentColor"
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            strokeWidth="2"
-            className="h-4 w-4 text-muted-foreground"
-          >
-            <path d="M12 2v20M17 5H9.5a3.5 3.5 0 0 0 0 7h5a3.5 3.5 0 0 1 0 7H6" />
-          </svg>
-        </CardHeader>
-        <CardContent>
-          <div className="text-2xl font-bold">
+          <CardTitle className="text-md font-bold">Total</CardTitle>
+          <span className="text-2xl font-bold">
+            {total > 0 ? "+" : ""}
             {total.toLocaleString("fr-FR")} €
-          </div>
+          </span>
+        </CardHeader>
+        <CardContent className="h-56">
+          <Incomes movements={movements} />
         </CardContent>
       </Card>
       <Card>
